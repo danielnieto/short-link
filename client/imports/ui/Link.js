7f6e70219de9947af5b69325440ea1f92f77f0a0
Daniel Nieto <danielnieto89@gmail.com>
2019-03-18 16:58:37 -0600

    add Signup and Link barebone components

---
+++ client/imports/ui/Link.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Signup extends React.Component {
+  render() {
+    return (
+      <p>
+        Link component
+      </p>
+    );
+  }
+}
--- client/main.js
+++ client/main.js
@@ -1,18 +1,12 @@
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Signup from './imports/ui/Signup';
+import Link from './imports/ui/Link';
 
 import './main.html';
-class Signup extends React.Component {
-  render() {
-    return (
-      <p>
-        Signup component here
-      </p>
-    );
-  }
-}
+
 
 Meteor.startup(()=> {
-  ReactDOM.render(<Signup />, document.getElementById('app'));
+  ReactDOM.render(<Link />, document.getElementById('app'));
 })
